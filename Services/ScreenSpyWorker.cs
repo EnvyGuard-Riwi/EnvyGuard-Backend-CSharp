b43b9287e5be747afeef97b0593ea7dac1b6c645
jrestrepo18 <jerorrpo@hmail.com>
2026-01-30 17:39:53 -0500

    fix: buscar XAUTHORITY dinámicamente

--- Services/ScreenSpyWorker.cs
+++ Services/ScreenSpyWorker.cs
@@ -118,7 +118,7 @@ public class ScreenSpyWorker : BackgroundService
             var psiScrot = new ProcessStartInfo
             {
                 FileName = "/bin/bash",
-                Arguments = $"-c \"export DISPLAY=:0 && export XAUTHORITY=/home/$USER/.Xauthority && scrot -z -o -q 50 '{tempFile}'\"",
+                Arguments = $"-c \"export DISPLAY=:0 && export XAUTHORITY=$(find /run/user/$(id -u)/ -name 'Xauthority' 2>/dev/null | head -n 1) && [ -z '$XAUTHORITY' ] && export XAUTHORITY=/home/$(whoami)/.Xauthority; scrot -z -o -q 50 '{tempFile}'\"",
                 UseShellExecute = false, 
                 CreateNoWindow = true,
                 RedirectStandardError = true,
